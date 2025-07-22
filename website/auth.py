486e9b709c7a39e7c526681fa5d59b8dab70f40d
CharaBLk <bellourakcharaf@gmail.com>
2025-07-22 19:17:58 +0200

    PUT POST methods

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>Sign up</p>'
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -5,3 +5,5 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+
