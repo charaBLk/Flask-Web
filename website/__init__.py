46b804e3748943fea4cac6826166fc7cf872bd9c
CharaBLk <bellourakcharaf@gmail.com>
2025-07-23 15:30:21 +0200

    flashed_messages error + db creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'njodezjnfjn'
     app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    
     db.init_app(app)
     
     from .views import views
@@ -17,7 +20,17 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth")
 
+    from .models import User, Note
+    
+    create_database(app)
 
 
     return app
 
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
+        
+        
